b424dc89ea9267e9afdba5f9f0039af432988e70
Vatsal Yadav <vatsalyad15@gmail.com>
2020-04-13 00:09:35 +0530

    Rename MainActivity file to WeatherDetailsActivity
    MainActivity file renamed and its references refactored in activity_photos_grid.xml and AndroidManifest.xml

---
+++ app/src/main/java/com/example/weather/view/WeatherDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.weather.view;
+
+import android.os.Bundle;
+
+import com.example.weather.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeatherDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
