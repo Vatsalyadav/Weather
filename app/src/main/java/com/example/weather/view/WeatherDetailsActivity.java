4d4ac549459e962c20284b418f96bf132e548130
Vatsal Yadav <vatsalyad15@gmail.com>
2020-04-14 00:21:04 +0530

    Update WeatherDetailsActivity to fetch Location from ViewModel
    WeatherDetailsActivity to fetch Location Details from ViewModel in a Life Cycle aware way and to implement ModelView View Model Architecture
    Add Fine Location permission to Android Manifest file

--- app/src/main/java/com/example/weather/view/WeatherDetailsActivity.java
+++ app/src/main/java/com/example/weather/view/WeatherDetailsActivity.java
@@ -2,34 +2,43 @@ package com.example.weather.view;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.weather.R;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.example.weather.models.Location;
+import com.example.weather.viewmodels.WeatherDetailsActivityViewModel;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 public class WeatherDetailsActivity extends AppCompatActivity {
     private static final String TAG = "WeatherDetailsActivity";
-    private FusedLocationProviderClient mFusedLocationClient;
     private int locationRequestCode = 1000;
-    private double latitude = 0.0, longitude = 0.0;
+    private WeatherDetailsActivityViewModel mWeatherDetailsActivityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_details);
+        displayToolbar();
+        initWeatherDetailsViewModel();
         initLocation();
     }
 
+    private void displayToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_weather_details);
+        setSupportActionBar(toolbar);
+    }
+
     private void initLocation() {
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // request permission
@@ -38,15 +47,27 @@ public class WeatherDetailsActivity extends AppCompatActivity {
 
         } else {
             // already permission granted
-            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        latitude = location.getLatitude();
-                        longitude = location.getLongitude();
-                    }
-                }
-            });
+            fetchLocation();
+        }
+    }
+
+    private void fetchLocation() {
+        mWeatherDetailsActivityViewModel.setLocationDetails();
+        mWeatherDetailsActivityViewModel.getLocationDetails().observe(this, new Observer<Location>() {
+            @Override
+            public void onChanged(@Nullable Location location) {
+                Log.e(TAG, "onChanged: " + location.getLatitude() + "  " + location.getLongitude());
+            }
+        });
+    }
+
+    private void initWeatherDetailsViewModel() {
+        try {
+            mWeatherDetailsActivityViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(WeatherDetailsActivityViewModel.class);
+            mWeatherDetailsActivityViewModel.initRepository();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Unable to get UI Data, please try again later.", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -58,15 +79,7 @@ public class WeatherDetailsActivity extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            if (location != null) {
-                                latitude = location.getLatitude();
-                                longitude = location.getLongitude();
-                            }
-                        }
-                    });
+                    fetchLocation();
                 } else {
                     Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
                 }
